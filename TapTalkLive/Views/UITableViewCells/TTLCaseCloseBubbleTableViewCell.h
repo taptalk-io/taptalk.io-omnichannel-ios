4aa3b0bbcb12286839ac5ee5f02a3c15578c9a32
Dominic Vedericho <>
2020-01-27 14:38:31 +0700

    add case close table view cell

---
+++ TapTalkLive/Views/UITableViewCells/TTLCaseCloseBubbleTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  TTLCaseCloseBubbleTableViewCell.h
+//  TapTalk
+//
+//  Created by Dominic Vedericho on 24/01/20.
+//  Copyright © 2020 TapTalk.io. All rights reserved.
+//
+
+#import <TapTalk/TAPBaseGeneralBubbleTableViewCell.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTLCaseCloseBubbleTableViewCell : TAPBaseGeneralBubbleTableViewCell
+
+- (void)setMessage:(TAPMessageModel *)message;
+
+@end
+
+NS_ASSUME_NONNULL_END
