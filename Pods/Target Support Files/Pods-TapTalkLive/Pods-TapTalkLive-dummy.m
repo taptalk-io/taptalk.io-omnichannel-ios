1e1ebabfc37284321def135faffb10e2f213e5e4
alfian0 <alfian.official.mail@gmail.com>
2020-11-30 20:16:48 +0700

    delete pod

--- Pods/Target Support Files/Pods-TapTalkLive/Pods-TapTalkLive-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TapTalkLive : NSObject
-@end
-@implementation PodsDummy_Pods_TapTalkLive
-@end
--- Pods/Target Support Files/TapTalk/TapTalk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TapTalk : NSObject
-@end
-@implementation PodsDummy_TapTalk
-@end
